92ce594b2ed17a09a3b762f0ceb78257b5b83819
Filip Kula <f.kula@1ins.pl>
2025-12-15 18:15:31 +0100

    refactor: remove comments from utils.ts for cleaner code

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,5 @@
-// src/utils.ts
-
-// Prosta implementacja funkcji pipe, która przekazuje dane z lewej do prawej
-// To pozwoli nam pisać kod w stylu: pipe(dane, funkcja1, funkcja2, funkcja3)
 export const pipe = <T>(val: T, ...fns: ((x: T) => T)[]): T => {
   return fns.reduce((prev, fn) => fn(prev), val);
 };
 
-// Pomocniczy typ dla predykatów (funkcji zwracających boolean)
 export type Predicate<T> = (item: T) => boolean;
